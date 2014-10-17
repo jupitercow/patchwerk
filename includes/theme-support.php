ba4ca003dcd3c582081dc93a58930f107743d63a
Jake Snyder <jacobsnyder@gmail.com>
2014-10-17 11:56:05 -0500

    Updated forms. fixed weird cursor placement. Added acf styles removal (commented out)

--- includes/theme-support.php
+++ includes/theme-support.php
@@ -34,6 +34,7 @@ function custom_register_sidebars()
 	) );
 }
 
+
 /**
  * Updating WordPress Functions & Theme Support
  *
@@ -86,3 +87,34 @@ function custom_theme_support()
 		'footer-nav' => 'Footer Navigation',   // links in footer
 	) );
 }
+
+
+/**
+ * Deregister admin styles on the front end when using ACF forms
+ * /
+add_action( 'wp_print_styles', 'custom_acf_deregister_styles', 100 );
+function custom_acf_deregister_styles()
+{
+	if (! is_admin() ) wp_deregister_style( 'wp-admin' );
+}
+
+/**
+ * Deregister most of the ACF default styles
+ * /
+add_action( 'wp_print_styles', 'remove_acf_styles', 200 );
+function remove_acf_styles()
+{
+	$styles = array(
+		'acf',
+		'acf-field-group',
+		'acf-pro-field-group',
+		'acf-global',
+		'acf-input',
+		'acf-pro-input',
+		'acf-datepicker',
+	);
+	foreach( $styles as $v ) {
+		wp_deregister_style( $v ); 
+	}
+}
+/**/
